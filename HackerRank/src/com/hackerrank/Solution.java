bbb320adfba0ee084065c1ac10fab23f24e04823
shravin <shravin@gmail.com>
2016-02-23 12:35:19 +0530

    Initial checkin test.

---
+++ HackerRank/src/com/hackerrank/Solution.java
@@ -0,0 +1,13 @@
+package com.hackerrank;
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(scanner.next());
+	}
+
+}
